a2693dc65fb60b9d99b8e8ddaa6659131cc9b135
JaroslavPejsa3 <129733962+JaroslavPejsa3@users.noreply.github.com>
2023-07-23 18:10:39 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,26 @@
+function skillsMember() {
+    var member = document.getElementById("member");
+    var skills = document.getElementById("skills");
+    var projects = document.getElementById("projects");
+    var contact = document.getElementById("contact");
+    var about = document.getElementById("about");
+    var home = document.getElementById("home");
+    var memberInfo = document.getElementById("memberInfo");
+    var skillsInfo = document.getElementById("skillsInfo");
+    var projectsInfo = document.getElementById("projectsInfo");
+    var contactInfo = document.getElementById("contactInfo");
+    var aboutInfo = document.getElementById("aboutInfo");
+    var homeInfo = document.getElementById("homeInfo");
+    member.style.color = "#3d3d3d";
+    skills.style.color = "#000000";
+    projects.style.color = "#000000";
+    contact.style.color = "#000000";
+    about.style.color = "#000000";
+    home.style.color = "#000000";
+    memberInfo.style.display = "block";
+    skillsInfo.style.display = "none";
+    projectsInfo.style.display = "none";
+    contactInfo.style.display = "none";
+    aboutInfo.style.display = "none";
+    homeInfo.style.display = "none";
+}
